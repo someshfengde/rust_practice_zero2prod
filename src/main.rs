06981d40919bd06c6e1ce0ac90a7d99e0a37f724
Somesh Fengade <someshfengde@gmail.com>
2024-02-15 16:22:32 +0530

    adding health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn greet() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn makeit() -> impl Responder {
+    HttpResponse::Ok().body("I am alive!")
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(|| {
+        App::new().route("/",web::get().to(greet))
+        .route("/hello", web::get().to(greet))
+        .route("/health_check" ,web::get().to(makeit))
+    })
+    .bind("127.0.0.1:8123")?
+    .run()
+    .await
 }
