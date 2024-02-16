4914e03b4bc1592d12664c92efee2b5b4450868c
Somesh Fengade <someshfengde@gmail.com>
2024-02-16 20:03:40 +0530

    removing comments

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,10 @@ async fn health_check() -> HttpResponse {
 pub fn run() -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            // .route("/", web::get().to(greet))
             .route("/hello", web::get().to(makeit))
             .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run();
-    // .await
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,3 @@
-// #[tokio::test]
-// async fn health_check_works() {
-//     spawn_app().await.expect("Failed to spawn our app.");
-//     let client = reqwest::Client::new();
-//     let response = client
-//         .get("http://127.0.0.1:8000/health_check")
-//         .send()
-//         .await
-//         .expect("Failed to execute request.");
-//     assert!(response.status().is_success());
-//     // assert_eq!("I am alive".to_string(), response.body)
-// }
 #[tokio::test]
 async fn health_check_works() {
     spawn_app(); //.await.expect("Failed to spawn our app.");
@@ -25,5 +13,4 @@ async fn health_check_works() {
 fn spawn_app() {
     let server = zero2prod::run().expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    // zero2prod::run().await
 }
